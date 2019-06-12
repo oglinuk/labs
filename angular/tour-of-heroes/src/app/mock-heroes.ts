5a5b4e53402887510adf67f11521ad2538380d86
OGLinuk <daniel-l-black@protonmail.com>
2019-06-13 00:00:09 +1200

    Added angular | Added python3 opencv example

---
+++ angular/tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Blackstorm' },
+    { id: 2, name: 'Bluestorm' },
+    { id: 3, name: 'Redstorm' },
+    { id: 4, name: 'Greenstorm' },
+    { id: 5, name: 'Yellowstorm' },
+    { id: 6, name: 'Cyanstorm' },
+    { id: 7, name: 'Purplestorm' },
+    { id: 8, name: 'Brownstorm' },
+    { id: 9, name: 'Whitestorm' },
+    { id: 10, name: 'Orangestorm' }
+];
